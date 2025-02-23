adaa986f0666ed2c6013a7dc985dd9cae8a87703
mitulgadhiya <88765850+mitullgadhiya@users.noreply.github.com>
2025-02-23 13:00:40 +0530

    mid

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,22 @@
-import { NextRequest } from "next/server"
+// import { NextRequest } from "next/server"
 
-const PUBLIC_FILE = /\.(.*)$/
+// const PUBLIC_FILE = /\.(.*)$/
 
-export const config = {
-  matcher: [
-    "/", // explicit matcher for root route
-    "/((?!_next/static).*)",
-  ],
-}
+// export const config = {
+//   matcher: [
+//     "/", // explicit matcher for root route
+//     "/((?!_next/static).*)",
+//   ],
+// }
 
-export async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl
+// export async function middleware(req: NextRequest) {
+//   const { pathname } = req.nextUrl
 
-  if (
-    pathname.startsWith("/_next") ||
-    pathname.includes("/api/") ||
-    PUBLIC_FILE.test(pathname)
-  ) {
-    return
-  }
-}
+//   if (
+//     pathname.startsWith("/_next") ||
+//     pathname.includes("/api/") ||
+//     PUBLIC_FILE.test(pathname)
+//   ) {
+//     return
+//   }
+// }
